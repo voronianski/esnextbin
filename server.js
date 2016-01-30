1e7719a5d4ab3ac47e4845ccd93b77948f1554b9
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-01-30 22:38:04 +0200

    remove some files, add ga

--- server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var port = 9292;
-var app = express();
-app.use(express.static(__dirname));
-app.listen(port);
-console.log('dev server started on localhost:%s', port);
