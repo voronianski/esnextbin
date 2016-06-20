f6f130f2087f9d155c8cfac0db15554f0bc4bcb0
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2016-06-20 13:51:25 +0200

    serve gatekeeper under https

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const envs = {
     production: {
         GITHUB_CLIENT_ID: '8d3a840b9412821160da',
-        GATEKEEPER: 'http://labs.voronianski.com/gatekeeper/token.php',
+        GATEKEEPER: 'https://labs.voronianski.com/gatekeeper/token.php',
         BROWSERIFY_CDN: 'https://wzrd.in'
     },
     dev: {
         GITHUB_CLIENT_ID: 'b861ee8ae510190dd2e8',
-        GATEKEEPER: 'http://labs.voronianski.com/gatekeeper-dev_/token.php',
+        GATEKEEPER: 'https://labs.voronianski.com/gatekeeper-dev_/token.php',
         BROWSERIFY_CDN: 'https://wzrd.in'
     }
 };
