e0c89cc6665d744a55575ca344a636c94ece7b82
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2018-11-14 17:30:23 +0100

    Updates

--- depUpdateHelper.js
+++
@@ -1,3 +0,0 @@
-const deps = {}; // put dependencies from package.json here
-
-console.log(Object.keys(deps).map(dep => `${dep}@latest`).join(' '));
--- devServer.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const port = process.env.PORT || 9292;
-const app = express();
-
-app.use(express.static(__dirname));
-app.listen(port);
-
-console.log('dev server started on http://localhost:%s', port);
